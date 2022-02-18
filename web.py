c9fd7e994b9071175f271e1d4263fa0e868a2ed4
alybenhur <alybenhur@gmail.com>
2022-02-18 16:56:30 -0500

    Update web.py

--- web.py
+++ web.py
@@ -72,7 +72,7 @@ def consulta():
         }""")
         unique_category = []
         for row in qres:
-           category = ("%s" % row)
+           category = ("%s" % row).rsplit('/',1)[-1]
            unique_category.append(category)
         #archivo = json.dumps(unique_category)
         #html = archivo
