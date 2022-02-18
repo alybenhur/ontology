c5d15735c89a60a5bd6030004ae173476bb1aa5b
alybenhur <alybenhur@gmail.com>
2022-02-18 13:10:21 -0500

    Update web.py

--- web.py
+++ web.py
@@ -53,7 +53,7 @@ def consulta():
     
   if consul == "1" :
        # html = """<p>>%s</p>""" % onto.Mat_Vis_Glo_M.instances()
-       d = (list(graph.query_owlready("""
+       d = str(list(graph.query_owlready("""
        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
        PREFIX owl: <http://www.w3.org/2002/07/owl#>
        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
