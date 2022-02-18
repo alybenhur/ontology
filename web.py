5c4d14c453ba80b32e4def28356e4d3b468a6b49
alybenhur <alybenhur@gmail.com>
2022-02-18 12:26:11 -0500

    Update web.py

--- web.py
+++ web.py
@@ -54,17 +54,16 @@ def consulta():
   if consul == "1" :
        # html = """<p>>%s</p>""" % onto.Mat_Vis_Glo_M.instances()
        d = (list(graph.query_owlready("""
-           PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-           PREFIX owl: <http://www.w3.org/2002/07/owl#>
-           PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-           PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-           PREFIX  :<http://www.semanticweb.org/miguelangel/ontologies/2022/estilos#>
-           SELECT   ?x ?y ?z ?w
-	        WHERE { 
-	        	?x   :se_identifica_con_e_a  :visual_secuencial_apropiado.
-		        ?x :formato  ?y.  
-		        ?x :nivel ?w.  
-                       }""")))
+           		PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+           		PREFIX owl: <http://www.w3.org/2002/07/owl#>
+         		PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+           		PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+           		PREFIX  :<http://www.semanticweb.org/miguelangel/ontologies/2022/estilos#>
+           		SELECT   ?x ?y ?z ?w WHERE { 
+	        		?x   :se_identifica_con_e_a  :visual_secuencial_apropiado.
+		        	?x :formato  ?y.  
+		       		?x :nivel ?w.  
+                        	  }""")))
          ruta = []
          
          for x in range(0,len(d)):
